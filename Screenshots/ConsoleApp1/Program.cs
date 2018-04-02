f54a4a57b44d0dce5354a29edc1e8e1b9c92cd0c
Jsquared <info@jsquaredsolutions.net>
2018-04-01 21:01:36 -0400

    Added the timer to take a screenshot every x seconds

--- Screenshots/ConsoleApp1/Program.cs
+++ Screenshots/ConsoleApp1/Program.cs
@@ -8,58 +8,49 @@ using System.Drawing.Imaging;
 using System.Windows.Forms;
 using System.IO; 
 
-
-
-
 namespace ConsoleApp1
-
 {
-
+   
     class Program
-
     {
-
         static public void Main()
         {
-            Application.Run(Main);
+            var timer = new System.Threading.Timer(
+      e => FullScreenshot(),
+      null,
+      TimeSpan.Zero,
+      TimeSpan.FromSeconds(10));
+            Console.ReadLine();
         }
-    
-        public static void MainProgram()
+
+        /* public static void MainProgram()
         {
             Timer t = new Timer(); // set the time (5 min in this case)
             t.Interval = 3000;
             t.Tick += new EventHandler(FullScreenshot);
             t.Start();
             ImageFormat.Jpeg
-        }
+        }*/
 
-        private static void FullScreenshot(Object myObject, EventArgs myEventArgs)
+        private static void FullScreenshot()
         {
-
             Rectangle bounds = Screen.GetBounds(Point.Empty);
-
             // If you want to look at a specific part of the screen us the below
             //bounds.Width = 70;
             //bounds.Height = 50;
-
             using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
-
             {
                 using (Graphics g = Graphics.FromImage(bitmap))
-
                 {
                     // If you want to look at a specific part of the screen us the below
                     // g.CopyFromScreen(new Point(339), new Point(33), bounds.Size);
                     g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
                 }                
-                string fullpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + "screenshot_" + DateTime.Now + ".jpg";
+                string fullpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\SS\\" + "screenshot_" + DateTime.Now.ToString("yyyyMMddHHmmss")+".jpeg";
                 bitmap.Save(fullpath, ImageFormat.Jpeg);
             }
-
         }
-
     }
-
 }
 
 
